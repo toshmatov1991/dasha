0ed4fc615c6dc22d23dc68df014d61406f511c7f
toshmatov1991 <kamentimur1991@gmail.com>
2023-12-01 12:21:34 +0700

    Редактирование таблицы

--- BaseWindow.xaml.cs
+++ BaseWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,11 +39,11 @@ namespace HumanResourcesDepartmentWPFApp
         {
             using OkContext ok = new();
 
-            dataGrid.ItemsSource =  ok.Personals.ToList();
+            dataGrid.ItemsSource = ok.Personals.ToList();
 
 
             //Заполнение Comboboxes
-            AreaCombobox =  [.. ok.Areas];
+            AreaCombobox = [.. ok.Areas];
             JobCombobox = [.. ok.JobTitles];
             SubCombobox = [.. ok.SubDivisions];
         }
@@ -68,11 +69,81 @@ namespace HumanResourcesDepartmentWPFApp
                 var upPer = await ok.Database.ExecuteSqlAsync($"UPDATE Personal SET family = {a.Family}, name = {a.Name}, lastname = {a.Lastname}, subDivision = {a.SubDivision}, jobTitle = {a.JobTitle}, adress = {a.Adress}, area = {a.Area}, inn = {a.Inn}, childrenCount = {a.ChildrenCount} WHERE id = {a.Id}");
                 if (upPer == 0)
                     MessageBox.Show("Произошла ошибка при обновлении таблицы(Заявитель)\nПовторите попытку");
-                
-                
-                
+            }
+        }
 
+        private async void UpdateSub(object sender, EventArgs e)
+        {
+            if ((dataGrid.SelectedItem as Personal)?.Id == 0 || (dataGrid.SelectedItem as Personal)?.Id == null)
+                return;
+            else
+            {
+                try
+                {
+                    //Меняем подразделение Заявителю
+                    using OkContext ok = new();
 
+                    var GetId = await ok.SubDivisions.AsNoTracking().Where(u => u.NameDivisions == (sender as ComboBox).Text).FirstOrDefaultAsync();
+
+                    if (GetId != null)
+                        await ok.Database.ExecuteSqlRawAsync("UPDATE Personal SET subDivision = {0} WHERE Id = {1}", GetId.Id, (dataGrid.SelectedItem as Personal)?.Id);
+                    else
+                        MessageBox.Show("Произошла ошибка при обновлении данных");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Произошла ошибка, повторите попытку", ex.Message);
+                }
+            }
+        }
+
+        private async void UpdateJob(object sender, EventArgs e)
+        {
+            if ((dataGrid.SelectedItem as Personal)?.Id == 0 || (dataGrid.SelectedItem as Personal)?.Id == null)
+                return;
+            else
+            {
+                try
+                {
+                    //Меняем подразделение Заявителю
+                    using OkContext ok = new();
+
+                    var GetId = await ok.JobTitles.AsNoTracking().Where(u => u.NameJobTitle == (sender as ComboBox).Text).FirstOrDefaultAsync();
+
+                    if (GetId != null)
+                        await ok.Database.ExecuteSqlRawAsync("UPDATE Personal SET jobTitle = {0} WHERE Id = {1}", GetId.Id, (dataGrid.SelectedItem as Personal)?.Id);
+                    else
+                        MessageBox.Show("Произошла ошибка при обновлении данных");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Произошла ошибка, повторите попытку", ex.Message);
+                }
+            }
+        }
+
+        private async void UpdateArea(object sender, EventArgs e)
+        {
+            if ((dataGrid.SelectedItem as Personal)?.Id == 0 || (dataGrid.SelectedItem as Personal)?.Id == null)
+                return;
+            else
+            {
+                try
+                {
+                    //Меняем подразделение Заявителю
+                    using OkContext ok = new();
+
+                    var GetId = await ok.Areas.AsNoTracking().Where(u => u.NameArea == (sender as ComboBox).Text).FirstOrDefaultAsync();
+
+                    if (GetId != null)
+                        await ok.Database.ExecuteSqlRawAsync("UPDATE Personal SET area = {0} WHERE Id = {1}", GetId.Id, (dataGrid.SelectedItem as Personal)?.Id);
+                    else
+                        MessageBox.Show("Произошла ошибка при обновлении данных");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Произошла ошибка, повторите попытку", ex.Message);
+                }
             }
         }
 }
